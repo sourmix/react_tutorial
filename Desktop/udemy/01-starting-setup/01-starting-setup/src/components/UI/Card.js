5fed245694ad1b14892ec9ef3bdc631dad4e0ffe
sourmix <75124027+sourmix@users.noreply.github.com>
2022-06-24 11:12:50 +0900

    2

---
+++ Desktop/udemy/01-starting-setup/01-starting-setup/src/components/UI/Card.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Card.css";
+
+const Card = (props) => {
+  const classess = "card " + props.className;
+  return <div className={classess}>{props.children}</div>;
+};
+
+export default Card;
